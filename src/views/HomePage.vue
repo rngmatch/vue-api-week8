623c4795e95a09e1fcf027c3a6dce48cfedb8510
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-03-06 15:11:39 -0600

    .

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,6 +4,24 @@ import { ref } from 'vue'
 </script>
 
 <template>
-<h1 class="text-6xl font-thin text-slate-800">I'm Home Page</h1>
-
+  <div class="brand">
+    <img class="logo" src="logo.svg" alt="logo" />
+    <h1 class="title">Triviatastic</h1>
+    <img class="logo" src="logo.svg" alt="logo" />
+  </div>
+  <div class="categories">
+<div v-for="n in 24" :key="n" class="category">Category - {{ n }}</div>
+</div>
 </template>
+
+<style lang="postcss" scoped>
+.categories {
+  @apply grid flex-grow grid-cols-4 gap-12;
+  & .category {
+    @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+    &:hover {
+      @apply cursor-pointer border-red-500 bg-red-500 text-white;
+    }
+  }
+}
+</style>
