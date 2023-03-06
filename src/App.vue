51cb51122cb255d5e1bae33fda559b5ed8dd1114
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-03-06 15:26:19 -0600

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <main>
     <div class="card">
-  <RouterView />
-  </div>
+      <RouterView />
+    </div>
   </main>
+  <p class="score">Score: 100</p>
 </template>
 
 <style lang="postcss" scoped>
 main {
-  @apply flex justify-center items-center h-screen bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900 first-letter::after;
+  @apply flex h-screen items-center justify-center bg-gradient-to-tr from-green-300 via-yellow-300 to-pink-300;
   & .card {
-    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-slate-400 p-8 shadow-lg;
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
   }
+}
+.score {
+  @apply absolute bottom-6 right-8 text-2xl font-bold uppercase text-slate-700;
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,20 +1,37 @@
 <script setup>
-import { ref } from 'vue'
-
+import { onMounted, ref } from 'vue'
+import useAPI from '@/composables/useAPI'
+const api = useAPI()
+const categories = ref([])
+onMounted(async () => {
+  categories.value = await api.getCategories()
+})
 </script>
 
 <template>
   <div class="brand">
     <img class="logo" src="logo.svg" alt="logo" />
-    <h1 class="title">Triviatastic</h1>
+    <h1 class="title">Triviantastic</h1>
     <img class="logo" src="logo.svg" alt="logo" />
   </div>
   <div class="categories">
-<div v-for="n in 24" :key="n" class="category">Category - {{ n }}</div>
-</div>
+    <RouterLink v-for="category in categories" :key="category.id" :to="`/question/category/${category.id}`"
+      class="category">
+      {{ category.name }}
+    </RouterLink>
+  </div>
 </template>
 
 <style lang="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+  & .logo {
+    @apply h-16 w-16;
+  }
+  & .title {
+    @apply text-6xl font-thin uppercase tracking-widest text-slate-700;
+  }
+}
 .categories {
   @apply grid flex-grow grid-cols-4 gap-12;
   & .category {
