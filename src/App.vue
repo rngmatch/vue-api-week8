183ce29c95eea25a4aefe32216bf09d2a796f1db
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-03-06 15:11:21 -0600

    not working

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
 </script>
 
 <template>
-  
+  <main>
+    <div class="card">
   <RouterView />
+  </div>
+  </main>
 </template>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex justify-center items-center h-screen bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900 first-letter::after;
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-slate-400 p-8 shadow-lg;
+  }
+}
+</style>
